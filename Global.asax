1c1a4bacd4d66c27493d70a03eb5c48e1d0006b8
Juhasz Zoltan <juhaszzoltanmate@yahoo.com>
2022-02-14 17:05:15 +0100

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASP220214V4.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP220214V4.Startup))]
+namespace ASP220214V4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
